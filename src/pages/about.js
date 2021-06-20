ccd1733f8917b35ab77b7bed1b8169624c7da3ed
Jessica Mejia <jessicamejia@mail.usf.edu>
2021-06-20 18:07:10 -0400

    styling react components

---
+++ src/pages/about.js
@@ -0,0 +1,19 @@
+import * as React from 'react'
+import { Link } from 'gatsby'
+
+const AboutPage = () => {
+    return (
+        <main>
+            <title>About Me</title>
+            <h1>About Me</h1>
+            <Link to="/">Back to Home</Link>
+            <p>hello! welcome to my page. I am the creator of this site which I built with Gatsby js!</p>
+
+            <h2>contact me</h2>
+            <p>contact through this website!</p>
+            <p>or here or there.</p>
+        </main>
+    )
+}
+
+export default AboutPage
